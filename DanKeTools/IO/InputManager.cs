96794317c8fe812ac5d4fede66310505e7be9cfd
DanKe <danke1024@foxmail.com>
2022-12-30 16:36:23 +0800

    update

---
+++ DanKeTools/IO/InputManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using Godot;
+using System;
+using System.Diagnostics;
+using DanKeTools;
+using DanKeTools.Event;
+using DanKeTools.Mono;
+
+namespace DanKeTools.IO
+{
+
+    ///<summary>
+    ///脚本名称： InputManager.cs
+    ///修改时间：2022/12/30
+    ///脚本功能：
+    ///备注：
+    ///</summary>
+
+    public class InputManager : Node
+    {
+        private bool isStart = false;
+
+        public override void _Ready()
+        {
+            GD.Print("请确保InputManager已挂载到AutoLoad作为单例运行！");
+        }
+        
+        
+        public override void _UnhandledInput(InputEvent @event)
+        {
+            if (@event is InputEventKey eventKey)
+            {
+                CheckKeyCode(eventKey , KeyList.A);
+            }
+        }
+
+        
+        
+        private void CheckKeyCode(InputEventKey eventKey, KeyList key)
+        {
+            if (GetKeyDown(eventKey,key))
+            {
+                DownTrigger(key);
+            }
+
+            if (GetKeyUp(eventKey, key))
+            {
+                UpTrigger(key);
+            }
+        }
+        private void DownTrigger(KeyList key)
+        {
+            if (isStart)
+            {
+                EventCenter.Instance().EventTrigger("KeyisDown", key);
+            }
+        }
+        private void UpTrigger(KeyList key)
+        {
+            if (isStart)
+            {
+                EventCenter.Instance().EventTrigger("KeyisUp", key);
+            }
+        }
+        private bool GetKeyDown(InputEventKey eventKey ,KeyList key)
+        {
+            if (eventKey.Pressed && eventKey.Scancode == (ulong)key)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool GetKeyUp(InputEventKey eventKey ,KeyList key)
+        {
+            if (eventKey.Pressed == false && eventKey.Scancode == (ulong)key)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+}
